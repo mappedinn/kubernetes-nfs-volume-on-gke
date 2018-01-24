107267210d53347c280ce3791c5ffeb05cd8d6f6
Amine Jallouli <jallouli.med.amine@gmail.com>
2018-01-24 07:58:03 +0400

    09-internal-dns-name

--- config-yml-files/00-run.sh
+++ config-yml-files/00-run.sh
@@ -4,14 +4,14 @@
 ###########################################################################################################
 
 # create a GCE persistent disk
-gcloud compute disks create --size=110GB --zone=us-east1-b gce-nfs-disk
+gcloud compute disks create --size=10GB --zone=us-east1-b gce-nfs-disk
 
 # create a GKE cluster
 # gcloud container clusters create mappedinn-cluster --machine-type=g1-small --num-nodes=1
-gcloud container clusters create mappedinn-cluster --num-nodes=1
+gcloud container clusters create mappedinn-cluster --num-nodes=1 --zone us-east1-b
 
 # changing the context of kubectl
-gcloud container clusters get-credentials mappedinn-cluster --zone us-east1-b --project mappedinn
+gcloud container clusters get-credentials mappedinn-cluster --zone us-east1-b --project amine-testing
 ## but it seems to be not necessary since after creating the cluster the context has been automatically changed
 
 kubectl create -f 01-dep-nfs.yml # have a look on https://kubernetes.io/docs/concepts/storage/volumes/#gcepersistentdisk
