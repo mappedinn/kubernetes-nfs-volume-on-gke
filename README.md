1ce22ce40645192d4a8738c3a2fef3f6e25ff03e
Amine Jallouli <jallouli.med.amine@gmail.com>
2018-01-26 19:49:26 +0400

    10-internal-dns-name-with-spec-correct-version-of-k8s

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 ## TL;DR
 Have a look directly on [./config-yml-files/00-run.sh](./config-yml-files/00-run.sh)
 
+## 0. Version of kubernetes on GKE cluster
+
+    # Make sure to have kubernetes with that version else errors related to DNS will occur
+    kubectl version
+    # Client Version: version.Info{Major:"1", Minor:"8", GitVersion:"v1.8.6", GitCommit:"6260bb08c46c31eea6cb538b34a9ceb3e406689c", GitTreeState:"clean", BuildDate:"2017-12-21T06:34:11Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}
+    # Server Version: version.Info{Major:"1", Minor:"8+", GitVersion:"v1.8.6-gke.0", GitCommit:"ee9a97661f14ee0b1ca31d6edd30480c89347c79", GitTreeState:"clean", BuildDate:"2018-01-05T03:36:42Z", GoVersion:"go1.8.3b4", Compiler:"gc", Platform:"linux/amd64"}
+    ## PS: by default, the kubectl on GKE is 1.7.11-gke.1
+    ## So, upgrade the GKE cluster to v1.8.6-gke.0
+    ## 1. Create a GKE cluster and GCE persistent disk
+
 ## 1. Create a GKE cluster and GCE persistent disk
 
     # create a GCE persistent disk
--- config-yml-files/00-run.sh
+++ config-yml-files/00-run.sh
@@ -3,6 +3,13 @@
 # run from the console through this command `sh ./00-run.sh`
 ###########################################################################################################
 
+# Make sure to have kubernetes with that version else errors related to DNS will occur
+kubectl version
+# Client Version: version.Info{Major:"1", Minor:"8", GitVersion:"v1.8.6", GitCommit:"6260bb08c46c31eea6cb538b34a9ceb3e406689c", GitTreeState:"clean", BuildDate:"2017-12-21T06:34:11Z", GoVersion:"go1.8.3", Compiler:"gc", Platform:"linux/amd64"}
+# Server Version: version.Info{Major:"1", Minor:"8+", GitVersion:"v1.8.6-gke.0", GitCommit:"ee9a97661f14ee0b1ca31d6edd30480c89347c79", GitTreeState:"clean", BuildDate:"2018-01-05T03:36:42Z", GoVersion:"go1.8.3b4", Compiler:"gc", Platform:"linux/amd64"}
+## PS: by default, the kubectl on GKE is 1.7.11-gke.1
+## So, upgrade the GKE cluster to v1.8.6-gke.0
+
 # create a GCE persistent disk
 gcloud compute disks create --size=10GB --zone=us-east1-b gce-nfs-disk
 
@@ -19,17 +26,18 @@ kubectl create -f 02-srv-nfs.yml
 kubectl create -f 03-pv-and-pvc-nfs.yml
 kubectl create -f 04-dep-busybox.yml
 
-# check if things are correctly working
+# check if things are correctly workiang
 kubectl exec nfs-busybox-2762569073-b2m99  -- cat /mnt/index.html
 
 # clean up the cluster (don't forget the clean up the cluster to not get charged)
-kubectl delete deployment nfs-busybox nfs-server
+kubectl delete deployment nfs-busybox
 kubectl delete service nfs-server
+kubectl delete deployment nfs-server
 kubectl delete pvc nfs
 kubectl delete pv nfs
 
 ## delete the cluser
-gcloud container clusters delete mappedinn-cluster
+gcloud container clusters delete mappedinn-cluster --zone us-east1-b
 
 ## deleting the GCE PV
-gcloud compute disks delete gce-nfs-disk
+gcloud compute disks delete gce-nfs-disk --zone us-east1-b
